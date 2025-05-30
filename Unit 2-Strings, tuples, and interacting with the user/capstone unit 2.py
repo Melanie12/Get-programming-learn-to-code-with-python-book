e0e0595f1c7ecdb367bcac09f9f849f5f1839e74
Melanie Almeida <12471571+Melanie12@users.noreply.github.com>
2025-05-30 22:10:41 +0200

    create capstone unit 2

---
+++ Unit 2-Strings, tuples, and interacting with the user/capstone unit 2.py
@@ -0,0 +1,15 @@
+#The problem
+#This is your first interactive programming task, so let's have some fun with the user! You want to write a program that automatically combines two names given by the user. 
+# That's an open-ended problem statement, so let's add a few more details and restrictions:
+#Tell the user to give you two names in the format: FIRST LAST
+#Show the user two possible new names in the format:FIRST LAST.
+#The new first name is a combination of the first names given by the user, and the new last name is a combination of the last names given by the user. 
+#For example, if the user gives you Alice Cat and Bob, a possible mashup is Bolice Dot.
+
+user_first=input("Give two names: ") #ask the user to give one name
+answer_1=user_first.upper() #make the answer uppercase
+print(answer_1)
+
+user_second=input("Give again two name: ") #ask the user to give one name
+answer_2=user_second.upper() #make the answer uppercase
+print(answer_2)
