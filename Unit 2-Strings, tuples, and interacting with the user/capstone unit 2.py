ec4b523a6442446e7d88cbc11bdfe94c25c81813
Melanie Almeida <12471571+Melanie12@users.noreply.github.com>
2025-06-04 22:04:57 +0200

    completed the capstone unit 2

--- Unit 2-Strings, tuples, and interacting with the user/capstone unit 2.py
+++ Unit 2-Strings, tuples, and interacting with the user/capstone unit 2.py
@@ -20,6 +20,7 @@ split_index_2=answer_2.find(" ")
 
 print(split_index_1) #finds the index of the space
 
+#split the first names and the last names
 first_name_1=answer_1[0:split_index_1]
 print(first_name_1)
 first_name_2=answer_2[0:split_index_2]
@@ -29,3 +30,17 @@ last_name_1=answer_1[split_index_1+1:len(answer_1)]
 print(last_name_1)
 last_name_2=answer_2[split_index_2+1:len(answer_2)]
 print(last_name_2)
+
+#split letters and combine them first name 1 and first name 2
+sub_string_first_name_1=first_name_1[0:1]
+sub_string_first_name_2=first_name_2[0:1]
+
+print(first_name_1.replace(sub_string_first_name_1,sub_string_first_name_2))
+print(first_name_2.replace(sub_string_first_name_2,sub_string_first_name_1))
+
+#split letters and combine them last name 1 and last name 2
+sub_string_last_name_1=last_name_1[len(last_name_1)-1:len(last_name_1)-2:-1]
+sub_string_last_name_2=last_name_2[len(last_name_1)-1:len(last_name_1)-2:-1]
+
+print(last_name_1.replace(sub_string_last_name_1,sub_string_last_name_2))
+print(last_name_2.replace(sub_string_last_name_2,sub_string_last_name_1))
