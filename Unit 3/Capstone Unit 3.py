6439891ff771d04e38d79ddc0d8ac0b730cc559a
Melanie Almeida <12471571+Melanie12@users.noreply.github.com>
2025-06-09 15:10:13 +0200

    Create Capstone Unit 3.py

---
+++ Unit 3/Capstone Unit 3.py
@@ -0,0 +1,7 @@
+# Capstone Unit 3
+
+#The problem
+# You'll use conditionals and branching to create a story. At each scene, the user will enter a word.
+# The word will tell the program which path to continue following.
+# Your program should handle all possible paths that the user might choose, but doesn't need to handle any unexpected input from the user.
+
