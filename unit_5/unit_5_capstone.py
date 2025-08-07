b49e7fff203a5ae083f30714158790f374666798
Melanie Almeida <contact@melanie-almeida.com>
2025-08-07 19:47:06 +0200

    adding the problem statement

---
+++ unit_5/unit_5_capstone.py
@@ -0,0 +1,5 @@
+# Analyze your friends
+
+# The problem
+#Write a program that reads input from a file in a specific format, regarding all your friends' names and phone numbers. Your program should store that information and analyze it in some way.
+# For example, you can show the user where their friends live based on the area code of the phone numbers, and the number of states where they live.
--- unit_5/unit_5_lessons.md
+++ unit_5/unit_5_lessons.md
@@ -1,3 +1,6 @@
-## What I learned in Unit 5
-### Functions
+# What I learned in Unit 5
+## Functions
 
+In this unit, I mostly learned about functions.
+
+Didn't know about the concept of Modularity and abstraction.
