e92081dea72689ab918311b64442a115582eae7c
Melanie Almeida <contact@melanie-almeida.com>
2025-10-03 10:06:17 +0200

    adding capstone unit 6

---
+++ unit_6/capstone_unit_6.py
@@ -0,0 +1,3 @@
+# The problem
+# You're given two files containing text. Using the names of the files, write a program that reads the documents and uses a metric to determine how similar they are.
+# Documents that are exactly the same should get a score of 1, and documents that don't have any words in common should get a score of 0.
